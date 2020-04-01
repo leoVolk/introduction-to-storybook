5873cf069dbf2ec1846a00892d4a8f070153ae99
Leonhard Volkmann <leonhardvolkmann@Pflegixs-MBP.localdomain>
2020-04-01 16:57:39 +0200

    little navbar icon

--- components/Navigation/Navbar.vue
+++ components/Navigation/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand href="/">{{title}}</b-navbar-brand>
+    <b-navbar-brand href="/">{{title}} <img src="https://image.flaticon.com/icons/svg/2755/2755189.svg" width="24px" height="24px"> </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
   </b-navbar>
